53d3d7c363db2df4d02a1778e3ff808b67f4f3d7
BriA827 <brigamer13@gmail.com>
2025-04-12 12:48:56 -0400

    continue working on knave

---
+++ portland_js/knave.js
@@ -0,0 +1,6 @@
+storyTitle = document.querySelector(".story_start")
+storyStuff = document.querySelector(".story_stuff")
+
+storyTitle.addEventListener("click", ()=>{
+    storyStuff.style.display = "flex"
+})
