ef4f5550bce9b0210eeaac1919a0a3ee2ba800cc
BriA827 <brigamer13@gmail.com>
2025-04-12 13:47:12 -0400

    c

--- portland_js/knave.js
+++ portland_js/knave.js
@@ -1,6 +1,12 @@
 storyTitle = document.querySelector(".story_start")
 storyStuff = document.querySelector(".story_stuff")
+storyEnd = document.querySelector(".story_end")
 
 storyTitle.addEventListener("click", ()=>{
     storyStuff.style.display = "flex"
+})
+
+storyEnd.addEventListener("click", ()=>{
+    storyStuff.style.display = "none"
+    document.documentElement.scrollTop = 20
 })
