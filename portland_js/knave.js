129c567a72ba87ab9829e4a75074f3d69d13e474
BriA827 <brigamer13@gmail.com>
2025-04-14 21:03:12 -0400

    c

--- portland_js/knave.js
+++ portland_js/knave.js
@@ -2,6 +2,9 @@ storyTitle = document.querySelector(".story_start")
 storyStuff = document.querySelector(".story_stuff")
 storyEnd = document.querySelector(".story_end")
 
+titleBtns = document.querySelectorAll(".title")
+infoPs = document.querySelectorAll(".info")
+
 storyTitle.addEventListener("click", ()=>{
     storyStuff.style.display = "flex"
     storyTitle.style.cursor = "default"
@@ -11,4 +14,14 @@ storyEnd.addEventListener("click", ()=>{
     storyStuff.style.display = "none"
     document.documentElement.scrollTop = 20
     storyTitle.style.cursor = "pointer"
+})
+
+titleBtns.forEach((titleBtn) => {
+    titleBtn.addEventListener("click", ()=>{
+        for (const info of infoPs) {
+            if (info.className.includes(titleBtn.textContent)){
+                info.style.display = "flex"
+            }
+        }
+    })
 })
