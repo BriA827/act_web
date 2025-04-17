a7ca61344461acc71c6d88f22a986634fc3f7a70
BriA827 <brigamer13@gmail.com>
2025-04-17 15:33:58 -0400

    c

--- portland_js/knave.js
+++ portland_js/knave.js
@@ -3,6 +3,7 @@ storyStuff = document.querySelector(".story_stuff")
 storyEnd = document.querySelector(".story_end")
 
 titleBtns = document.querySelectorAll(".down_arrow")
+closeBtns = document.querySelectorAll(".up_arrow")
 infoPs = document.querySelectorAll(".info")
 
 storyTitle.addEventListener("click", ()=>{
@@ -21,6 +22,28 @@ titleBtns.forEach((titleBtn) => {
         for (const info of infoPs) {
             if (info.id.includes(titleBtn.id)){
                 info.style.display = "flex"
+                titleBtn.style.display = "none"
+                for (const close of closeBtns) {
+                    if (close.id.includes(info.id)){
+                        close.style.display = "flex"
+                    }
+                }
+            }
+        }
+    })
+})
+
+closeBtns.forEach((closeBtn) => {
+    closeBtn.addEventListener("click", ()=>{
+        for (const info of infoPs) {
+            if (info.id.includes(closeBtn.id)){
+                info.style.display = "none"
+                closeBtn.style.display = "none"
+                for (const titleBtn of titleBtns) {
+                    if (titleBtn.id.includes(info.id)){
+                        titleBtn.style.display = "flex"
+                    }
+                }
             }
         }
     })
